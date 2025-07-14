e89652af3fa27a5d3544859821a8511ad28db7f7
Vic428-human <z0983195379a@gmail.com>
2025-07-14 15:17:01 +0800

    first commit

---
+++ lib-ui/src/App.tsx
@@ -0,0 +1,12 @@
+// 這個是直接在本地引用
+import { Button } from "house-ui";
+
+function App() {
+  return (
+    <>
+      <Button>!sdsad</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ lib-ui/vite.config.ts
@@ -0,0 +1,34 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import react from "@vitejs/plugin-react";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    // library entry and output settings
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "lib-ui",
+      fileName: "lib-ui",
+    },
+    // bundler options
+    // externalize react-related imports
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
+
+// Rollup 是一个 JavaScript 模块打包器，特别适合用于库和应用的打包
